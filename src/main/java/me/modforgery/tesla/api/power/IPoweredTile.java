d1c0351596fae6f1969ce0cc62a09d23728a297f
xbony2 <xbony2@users.noreply.github.com>
2015-05-20 19:00:55 -0400

    Update IPoweredTile.java

--- src/main/java/me/modforgery/tesla/api/power/IPoweredTile.java
+++ src/main/java/me/modforgery/tesla/api/power/IPoweredTile.java
@@ -1,5 +1,10 @@
 package me.modforgery.tesla.api.power;
 
+/**
+ * Anything implementing this class should receive
+ * energy/plasma.
+ *
+ */
 public interface IPoweredTile {
 
     public PoweredTile getPoweredTile();
