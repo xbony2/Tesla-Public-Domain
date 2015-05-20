88d7aaf6ce5ad4086b8ce334ea584da832b2bb76
xbony2 <xbony2@users.noreply.github.com>
2015-05-20 17:49:40 -0400

    Create CopperIngot.java

---
+++ src/main/java/me/modforgery/tesla/item/CopperIngot.java
@@ -0,0 +1,19 @@
+package me.modforgery.tesla.item;
+
+import me.modforgery.tesla.CreativeTabTesla;
+import net.minecraft.item.Item;
+
+/**
+ * File by ModForgery for Minecraft on 13/09/2014.
+ * 
+ * Created by xbony2 for ModForgery :p
+ * May they distribute this code forever
+ */
+public class CopperIngot extends Item{
+    public CopperIngot(){
+        super();
+        this.setCreativeTab(CreativeTabTesla.TS_TAB);
+        this.setUnlocalizedName("copper_ingot");
+        this.setTextureName("tesla:copper_ingot");
+    }
+}
