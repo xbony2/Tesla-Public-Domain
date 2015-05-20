284b75349f354062106a948690308912a8054f11
xbony2 <xbony2@users.noreply.github.com>
2015-05-20 17:44:43 -0400

    Create Tesla.java

---
+++ src/main/java/me/modforgery/tesla/Tesla.java
@@ -0,0 +1,11 @@
+import me.modforgery.tesla.crossmod.TeslaCrossMod;
+
+import me.modforgery.tesla.init.TeslaOreDictionary;
+
+    TeslaCrossMod.preInit();
+    
+    TeslaCrossMod.init();
+    
+    TeslaOreDictionary.init();
+    
+    TeslaCrossMod.postInit();
