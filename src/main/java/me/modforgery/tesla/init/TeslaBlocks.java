d0cc2070fe31eee6f292b8630fe2160396f987bd
xbony2 <xbony2@users.noreply.github.com>
2015-05-20 18:52:40 -0400

    Update TeslaBlocks.java

--- src/main/java/me/modforgery/tesla/init/TeslaBlocks.java
+++ src/main/java/me/modforgery/tesla/init/TeslaBlocks.java
@@ -1,3 +1,15 @@
 import me.modforgery.tesla.block.te.TileEntityPlasmaFurnace;
 
+        public static Block oreTin;
+        public static Block blockCopper;
+        public static Block blockTin;
+        
+	oreTin = new BlockTinOre();
+	blockCopper = new BlockDecorative("blockCopper");
+	blockTin = new BlockDecorative("blockTin");
+	
+	GameRegistry.registerBlock(oreTin, "oreTin");
+	GameRegistry.registerBlock(blockCopper, "blockCopper");
+	GameRegistry.registerBlock(blockTin, "blockTin");
+
         GameRegistry.registerTileEntity(TileEntityPlasmaFurnace.class, "te_plasmaFurnace");
