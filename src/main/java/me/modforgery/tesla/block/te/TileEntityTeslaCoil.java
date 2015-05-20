5776ca7589a780e4b95ec4628ec875f376a03744
xbony2 <xbony2@users.noreply.github.com>
2015-05-20 17:41:35 -0400

    Update TileEntityTeslaCoil.java

--- src/main/java/me/modforgery/tesla/block/te/TileEntityTeslaCoil.java
+++ src/main/java/me/modforgery/tesla/block/te/TileEntityTeslaCoil.java
@@ -2,6 +2,15 @@ import cpw.mods.fml.common.FMLCommonHandler;
 import cpw.mods.fml.relauncher.Side;
 import me.modforgery.tesla.client.particles.TeslaParticles;
 
+public class TileEntityTeslaCoil extends TileEntity implements IPoweredTile {
+    
+    //This is the powered tile instance
+    PoweredTile poweredTile;
+    
+    public TileEntityTeslaCoil() {
+        poweredTile = new PoweredTile(10000);
+    }
+
     @Override
     public void updateEntity(){
        super.updateEntity();
@@ -10,12 +19,23 @@ import me.modforgery.tesla.client.particles.TeslaParticles;
                 for (int z = -scanRadius; z < scanRadius; z++) {
                    TileEntity tile = this.worldObj.getTileEntity(this.xCoord + x, this.yCoord + y, this.zCoord + z);
                    if(tile != null && tile instanceof IPoweredTile){
-                       if (FMLCommonHandler.instance().getSide() == Side.CLIENT){
-                           TeslaParticles.plasa(this.worldObj, this.xCoord + 0.5, this.yCoord + 0.3, this.zCoord + 0.5 , tile.xCoord + 0.5, tile.yCoord + 0.5, tile.zCoord + 0.5);
+                       if(tile instanceof TileEntityTeslaCoil){
+                           //TODO balace the power here
+                           //TODO Find out how meny it is connected to here
+                       }else {
+                           //TODO move the power here to all of the powered tiles
+                           if (FMLCommonHandler.instance().getSide() == Side.CLIENT){
+                               TeslaParticles.plasa(this.worldObj, this.xCoord + 0.5, this.yCoord + 0.3, this.zCoord + 0.5 , tile.xCoord + 0.5, tile.yCoord + 0.5, tile.zCoord + 0.5);
+                           }
                        }
-
                    }
                 }
             }
         }
     }
+    
+    @Override
+    public PoweredTile getPoweredTile() {
+        return poweredTile;
+    }
+}
