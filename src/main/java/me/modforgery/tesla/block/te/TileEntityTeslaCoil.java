b07795ea91f4112a8363c83c11258d809b15c23a
xbony2 <xbony2@users.noreply.github.com>
2015-05-20 17:32:34 -0400

    Update TileEntityTeslaCoil.java

--- src/main/java/me/modforgery/tesla/block/te/TileEntityTeslaCoil.java
+++ src/main/java/me/modforgery/tesla/block/te/TileEntityTeslaCoil.java
@@ -5,6 +5,17 @@ import me.modforgery.tesla.client.particles.TeslaParticles;
     @Override
     public void updateEntity(){
        super.updateEntity();
-        if (FMLCommonHandler.instance().getSide() == Side.CLIENT)
-            TeslaParticles.plasa(this.worldObj, this.xCoord + 0.5, this.yCoord + 0.3, this.zCoord + 0.5 , this.xCoord + 0.5 + 10, this.yCoord + 0.3, this.zCoord + 0.5);
+        for (int x = -scanRadius; x < scanRadius; x++) {
+            for (int y = -scanRadius; y < scanRadius; y++) {
+                for (int z = -scanRadius; z < scanRadius; z++) {
+                   TileEntity tile = this.worldObj.getTileEntity(this.xCoord + x, this.yCoord + y, this.zCoord + z);
+                   if(tile != null && tile instanceof IPoweredTile){
+                       if (FMLCommonHandler.instance().getSide() == Side.CLIENT){
+                           TeslaParticles.plasa(this.worldObj, this.xCoord + 0.5, this.yCoord + 0.3, this.zCoord + 0.5 , tile.xCoord + 0.5, tile.yCoord + 0.5, tile.zCoord + 0.5);
+                       }
+
+                   }
+                }
+            }
+        }
     }
