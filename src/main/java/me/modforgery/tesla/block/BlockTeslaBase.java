7fc86c2251706f6379b210a3542ddfb2fd74d74e
xbony2 <xbony2@users.noreply.github.com>
2015-05-20 17:18:42 -0400

    Create BlockTeslaBase.java

---
+++ src/main/java/me/modforgery/tesla/block/BlockTeslaBase.java
@@ -0,0 +1,20 @@
+    /**
+     * Called whenever the block is added into the world. Args: world, x, y, z
+     */
+    public void onBlockAdded(World world, int x, int y, int z)
+    {
+        super.onBlockAdded(world, x, y, z);
+    }
+
+    public void breakBlock(World world, int x, int y, int z, Block block, int meta)
+    {
+        super.breakBlock(world, x, y, z, block, meta);
+        world.removeTileEntity(x, y, z);
+    }
+
+    public boolean onBlockEventReceived(World world, int x, int y, int z, int p_149696_5_, int p_149696_6_)
+    {
+        super.onBlockEventReceived(world, x, y, z, p_149696_5_, p_149696_6_);
+        TileEntity tileentity = world.getTileEntity(x, y, z);
+        return tileentity != null ? tileentity.receiveClientEvent(p_149696_5_, p_149696_6_) : false;
+    }
