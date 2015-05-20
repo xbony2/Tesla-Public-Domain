a20982512427899b94a5db208f822e62761425f4
xbony2 <xbony2@users.noreply.github.com>
2015-05-20 17:47:41 -0400

    Create TeslaCrossMod.java

---
+++ src/main/java/me/modforgery/tesla/crossmod/TeslaCrossMod.java
@@ -0,0 +1,31 @@
+package me.modforgery.tesla.crossmod;
+
+/**
+ * File by ModForgery for Minecraft on 13/09/2014.
+ * 
+ * Created by xbony2 for ModForgery :p
+ * May they distribute this code forever
+ */
+public class TeslaCrossMod {
+	
+	/**
+	 * Pre init should ONLY be used to detect if 
+	 * other mods are detected. Adding/changing
+	 * stuff should happen in init or postinit,
+	 * depending on what you want to add.
+	 * 
+	 * @see <a href="https://github.com/xbony2/IndustrialEx/blob/master/src/main/java/xbony2/IndustrialEx/crossmod/CrossMod.java">A good example</a>
+	 * 
+	 */
+	public static void preInit(){
+		
+	}
+	
+	public static void init(){
+		
+	}
+	
+	public static void postInit(){
+		
+	}
+}
