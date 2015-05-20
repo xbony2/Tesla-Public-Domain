e53cceefcbabcfe7a7919ea7a7c6ac8cf24322f7
xbony2 <xbony2@users.noreply.github.com>
2015-05-20 19:03:11 -0400

    Update TeslaItems.java

--- src/main/java/me/modforgery/tesla/init/TeslaItems.java
+++ src/main/java/me/modforgery/tesla/init/TeslaItems.java
@@ -4,14 +4,21 @@ import me.modforgery.tesla.item.TVoltmeter;
 
 import cpw.mods.fml.common.registry.GameRegistry;
 
+public class TeslaItems{
+
   public static Item copper_ingot;
   public static Item tin_ingot;
   
-    copper_ingot = new CopperIngot();
-    tin_ingot = new TinIngot();
+  public static void init(){
+    conductor = new CraftingItem("conductor");
+    coil = new CraftingItem("coil");
+    copper_ingot = new CraftingItem("copper_ingot");
+    tin_ingot = new CraftingItem("tin_ingot");
     
     GameRegistry.registerItem(conductor, "Conductor");
     
     GameRegistry.registerItem(copper_ingot, "copper_ingot");
     
     GameRegistry.registerItem(tin_ingot, "tin_ingot");
+  }
+}
