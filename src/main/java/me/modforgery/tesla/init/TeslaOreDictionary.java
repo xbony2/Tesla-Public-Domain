d4833cf56d3de7a74592c85e796a20891aff5292
xbony2 <xbony2@users.noreply.github.com>
2015-05-20 18:54:08 -0400

    Update TeslaOreDictionary.java

--- src/main/java/me/modforgery/tesla/init/TeslaOreDictionary.java
+++ src/main/java/me/modforgery/tesla/init/TeslaOreDictionary.java
@@ -12,7 +12,11 @@ public class TeslaOreDictionary {
 
 	public static void init(){
 		OreDictionary.registerOre("oreCopper", TeslaBlocks.oreCopper);
+		OreDictionary.registerOre("oreTin", TeslaBlocks.oreTin);
+		OreDictionary.registerOre("blockCopper", TeslaBlocks.blockCopper);
+		OreDictionary.registerOre("blockTin", TeslaBlocks.blockTin);
 		
 		OreDictionary.registerOre("ingotCopper", TeslaItems.copper_ingot);
+		OreDictionary.registerOre("ingotTin", TeslaItems.tin_ingot);
 	}
 }
