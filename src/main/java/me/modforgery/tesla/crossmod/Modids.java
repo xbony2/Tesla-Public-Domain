b79e69dacb30dfc59418a617035145a8ee802910
xbony2 <xbony2@users.noreply.github.com>
2015-05-20 17:47:03 -0400

    Create Modids.java

---
+++ src/main/java/me/modforgery/tesla/crossmod/Modids.java
@@ -0,0 +1,13 @@
+package me.modforgery.tesla.crossmod;
+/**
+ * File by ModForgery for Minecraft on 13/09/2014.
+ * 
+ * Created by xbony2 for ModForgery :p
+ * May they distribute this code forever
+ */
+public class Modids {
+public static final String IC2_ID = "IC2";
+public static final String BUILDCRAFT_ID = "BuildCraft|Core";
+public static final String COFHCORE_ID = "CoFHCore";
+public static final String GREGTECH_ID = "gregtech";
+}
