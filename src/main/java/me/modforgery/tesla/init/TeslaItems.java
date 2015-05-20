0d0acfed926ef09ee1fbb58481bf163586ec4387
xbony2 <xbony2@users.noreply.github.com>
2015-05-20 18:53:31 -0400

    Update TeslaItems.java

--- src/main/java/me/modforgery/tesla/init/TeslaItems.java
+++ src/main/java/me/modforgery/tesla/init/TeslaItems.java
@@ -1,7 +1,13 @@
-import me.modforgery.tesla.item.CopperIngot;
+import me.modforgery.tesla.item.*;
 
   public static Item copper_ingot;
+  public static Item tin_ingot;
   
     copper_ingot = new CopperIngot();
+    tin_ingot = new TinIngot();
+    
+    GameRegistry.registerItem(conductor, "Conductor");
     
     GameRegistry.registerItem(copper_ingot, "copper_ingot");
+    
+    GameRegistry.registerItem(tin_ingot, "tin_ingot");
