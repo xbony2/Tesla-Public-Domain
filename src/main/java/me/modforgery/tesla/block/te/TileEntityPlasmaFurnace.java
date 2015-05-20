5c586857f997c6df5ee5c38ae28e691bf28d7152
xbony2 <xbony2@users.noreply.github.com>
2015-05-20 17:38:46 -0400

    Update TileEntityPlasmaFurnace.java

--- src/main/java/me/modforgery/tesla/block/te/TileEntityPlasmaFurnace.java
+++ src/main/java/me/modforgery/tesla/block/te/TileEntityPlasmaFurnace.java
@@ -1,14 +1,14 @@
 package me.modforgery.tesla.block.te;
 
 import me.modforgery.tesla.api.power.IPoweredTile;
-import me.modforgery.tesla.api.power.PowedTile;
+import me.modforgery.tesla.api.power.PoweredTile;
 import net.minecraft.tileentity.TileEntity;
 
 public class TileEntityPlasmaFurnace extends TileEntity implements IPoweredTile{
 
-    PowedTile powedTile = new PowedTile(1000);
+    PoweredTile poweredTile = new PoweredTile(1000);
     
     @Override
-    public PowedTile getPowedTile() {
-        return powedTile;
+    public PoweredTile getPoweredTile() {
+        return poweredTile;
     }
