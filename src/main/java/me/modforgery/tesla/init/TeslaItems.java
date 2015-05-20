e036a99b21ddbd1c029db6efcd0ded4ac1ba52ea
xbony2 <xbony2@users.noreply.github.com>
2015-05-20 17:48:38 -0400

    Create TeslaItems.java

---
+++ src/main/java/me/modforgery/tesla/init/TeslaItems.java
@@ -0,0 +1,7 @@
+import me.modforgery.tesla.item.CopperIngot;
+
+  public static Item copper_ingot;
+  
+    copper_ingot = new CopperIngot();
+    
+    GameRegistry.registerItem(copper_ingot, "copper_ingot");
